6c2511cb0cbfbd496bcc846583e6bcf2923d522b
Selim Cebecioglu <selimcebecioglu@gmail.com>
2016-08-31 20:25:51 -0400

    Revert "Events feature and MTA alerts- merge me"

--- app/web/db.js
+++
@@ -1,9 +0,0 @@
-var dbURI = require('./config/config.js');
-var mongoose = require("mongoose");
-
-var dbURL = process.env.DBURL || dbURI;
-
-module.exports = mongoose.connect(dbURL, function(err) {
-  if (err) console.error(err);
-  else console.log("Successfully connected to database");
-});
