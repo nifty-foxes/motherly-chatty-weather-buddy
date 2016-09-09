a9cfdfb3668a9cfbe366b6f96b93c500c8e573c7
Jack <jisapp02@gmail.com>
2016-09-09 16:42:36 -0400

    test framework in place

--- app/test/test.js
+++ app/test/test.js
@@ -1,9 +1,20 @@
 var assert = require('assert');
+var server = require('../web/server.js');
+var supertest = require('supertest');
+var request = supertest.agent(server);
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+describe('Example', function() {
+  it('should return -1 when the value is not present', function() {
+    assert.equal(-1, [1,2,3].indexOf(4));
+  });
+});
+
+
+describe('Server', function() {
+  it('should return the content of index.html', function (done) {
+    // just assume that if it contains an <input> tag its index.html
+    request
+      .get('/api/transportation/')
+      .expect(200, /<input/, done);
   });
 });
